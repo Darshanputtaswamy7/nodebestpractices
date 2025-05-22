05d6c667de9b980d98a45a26f4037bf30a483a0e
Darshanputtaswamy7 <darshanputtaswamy7@gmail.com>
2025-05-22 09:09:44 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 # Required
 sonar.projectKey=your_project_unique_key  # e.g., "myapp-backend"
 sonar.projectName=Your Project Name
-sonar.host.url=http://43.205.253.7:9000/
+sonar.host.url=http://172.31.34.232:9000/
 sonar.login=squ_e466595a4745ca2a80c1ff0741ae29f6946bc5d1
 
 # Source files
